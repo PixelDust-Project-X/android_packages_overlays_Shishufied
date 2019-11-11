d268de2b9bf2acd7f81984b639c0a4adb5397637
El Dainosor <eldainosor@gmail.com>
2019-11-11 06:24:31 +0000

    Themes: Add an alpha release of Shishu Dreams
    Probably with a lot of broken things, we're still working on it. 10 added a lot of shaenanigans
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- shishu.mk
+++ shishu.mk
@@ -4,3 +4,131 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
 	Shishufied
+<<<<<<< HEAD
+=======
+
+# Theme-Required overlays packages
+PRODUCT_PACKAGES += \
+    ShishuThemeAndroidOverlay \
+    ShishuThemeSettingsOverlay \
+    ShishuThemeSystemUIOverlay \
+
+# Hot garbage
+#PRODUCT_PACKAGES += \
+#    DarkThemeAndroidOverlay \
+#    DarkThemeContactsOverlay \
+#    DarkThemeDialerOverlay \
+#    DarkThemeFilesOverlay \
+#    DarkThemeOTAOverlay \
+#    DarkThemePhonographOverlay \
+#    DarkThemeSettingsOverlay \
+#    DarkThemeSystemUIOverlay \
+#    BlackThemeAndroidOverlay \
+#    BlackThemeContactsOverlay \
+#    BlackThemeDialerOverlay \
+#    BlackThemeFilesOverlay \
+#    BlackThemeOTAOverlay \
+#    BlackThemePhonographOverlay \
+#    BlackThemeSettingsOverlay \
+#    BlackThemeSystemUIOverlay \
+#    ShishuThemeContactsOverlay \
+#    ShishuThemeDialerOverlay \
+#    ShishuThemeFilesOverlay \
+#    ShishuThemeGMSOverlay \
+#    ShishuThemeOTAOverlay \
+#    ShishuThemePhonographOverlay \
+#    ShishuThemeWellbeingOverlay \
+#    ShishuNightsThemeAndroidOverlay \
+#    ShishuNightsThemeContactsOverlay \
+#    ShishuNightsThemeDialerOverlay \
+#    ShishuNightsThemeFilesOverlay \
+#    ShishuNightsThemeGMSOverlay \
+#    ShishuNightsThemeOTAOverlay \
+#    ShishuNightsThemePhonographOverlay \
+#    ShishuNightsThemeSettingsOverlay \
+#    ShishuNightsThemeSystemUIOverlay \
+#    ShishuNightsThemeWellbeingOverlay \
+#    ShishuIllusionsThemeAndroidOverlay \
+#    ShishuIllusionsThemeContactsOverlay \
+#    ShishuIllusionsThemeDialerOverlay \
+#    ShishuIllusionsThemeFilesOverlay \
+#    ShishuIllusionsThemeGMSOverlay \
+#    ShishuIllusionsThemeOTAOverlay \
+#    ShishuIllusionsThemePhonographOverlay \
+#    ShishuIllusionsThemeSettingsOverlay \
+#    ShishuIllusionsThemeSystemUIOverlay \
+#    ShishuIllusionsThemeWellbeingOverlay \
+#    ShishuImmensityThemeAndroidOverlay \
+#    ShishuImmensityThemeContactsOverlay \
+#    ShishuImmensityThemeDialerOverlay \
+#    ShishuImmensityThemeFilesOverlay \
+#    ShishuImmensityThemeGMSOverlay \
+#    ShishuImmensityThemeOTAOverlay \
+#    ShishuImmensityThemePhonographOverlay \
+#    ShishuImmensityThemeSettingsOverlay \
+#    ShishuImmensityThemeSystemUIOverlay \
+#    ShishuImmensityThemeWellbeingOverlay \
+#    ShishuAmalgamationThemeAndroidOverlay \
+#    ShishuAmalgamationThemeContactsOverlay \
+#    ShishuAmalgamationThemeDialerOverlay \
+#    ShishuAmalgamationThemeFilesOverlay \
+#    ShishuAmalgamationThemeGMSOverlay \
+#    ShishuAmalgamationThemeOTAOverlay \
+#    ShishuAmalgamationThemePhonographOverlay \
+#    ShishuAmalgamationThemeSettingsOverlay \
+#    ShishuAmalgamationThemeSystemUIOverlay \
+#    ShishuAmalgamationThemeWellbeingOverlay \
+#    ShishuCosmosThemeAndroidOverlay \
+#    ShishuCosmosThemeContactsOverlay \
+#    ShishuCosmosThemeDialerOverlay \
+#    ShishuCosmosThemeFilesOverlay \
+#    ShishuCosmosThemeGMSOverlay \
+#    ShishuCosmosThemeOTAOverlay \
+#    ShishuCosmosThemePhonographOverlay \
+#    ShishuCosmosThemeSettingsOverlay \
+#    ShishuCosmosThemeSystemUIOverlay \
+#    ShishuCosmosThemeWellbeingOverlay \
+#    ShishuProtostarThemeAndroidOverlay \
+#    ShishuProtostarThemeContactsOverlay \
+#    ShishuProtostarThemeDialerOverlay \
+#    ShishuProtostarThemeFilesOverlay \
+#    ShishuProtostarThemeGMSOverlay \
+#    ShishuProtostarThemeOTAOverlay \
+#    ShishuProtostarThemePhonographOverlay \
+#    ShishuProtostarThemeSettingsOverlay \
+#    ShishuProtostarThemeSystemUIOverlay \
+#    ShishuProtostarThemeWellbeingOverlay
+#
+## Wallpaper overlay selection for widescreen devices.
+#ifeq ($(BOOTLEGGERS_NOTCHED),true)
+#    DEVICE_PACKAGE_OVERLAYS += \
+#        packages/overlays/Shishufied/Themes/overlay/widebois
+#endif
+#
+##Quicksettings Tiles icons
+#PRODUCT_PACKAGES += \
+#	QSTileSquircle \
+#	QSTileTearDrop \
+#	QSTileCircleDual \
+#	QSTileCircleGradient \
+#	QSTileInkdrop \
+#	QSTileIconAccent \
+#	QSTileShishuNights \
+#	QSTileMemedoSquare \
+#	QSTileWavey \
+#	QSTileAttemptMountain \
+#	QSTileDottedCircle \
+#	QSTileNinja \
+#	QSTilePokesign \
+#	QSTileShishuInk \
+#    QSTileCookie \
+#    QSTileCosmos \
+#    QSTileDividedCircle \
+#    QSTileNeonLight \
+#    QSTileOxygen \
+#    QSTileTriangles
+## DO NOT MERGE - 
+#PRODUCT_PACKAGES += \
+#    ShishuTestOv \
+#    ShishuTestOv2 
+>>>>>>> 7bd9fb4... Themes: Add an alpha release of Shishu Dreams
