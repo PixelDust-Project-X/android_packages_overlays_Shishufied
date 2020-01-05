dfa8cf2d42fe0b08dfd53e78ae3b53449d7d685d
spezi77 <spezi7713@gmx.net>
2020-01-05 10:23:50 +0100

    Allow overlays to be excluded from enforcing RRO.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- shishu.mk
+++ shishu.mk
@@ -1,3 +1,6 @@
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += packages/overlays/Shishufied/Overlays
+
 # Adding extras to our needs related to ThemePicker
 DEVICE_PACKAGE_OVERLAYS += \
     packages/overlays/Shishufied/Overlays
